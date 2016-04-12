2262a6b0247258dce612fb7776539650eaa7dd09
devilmustcry <eaglization@gmail.com>
2016-04-12 09:46:16 +0400

    Done with prototype Login layout

--- app/src/main/java/com/sandstorm/softspec/queue2win/Activities/LoginActivity.java
+++ app/src/main/java/com/sandstorm/softspec/queue2win/Activities/LoginActivity.java
@@ -2,14 +2,32 @@ package com.sandstorm.softspec.queue2win.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.sandstorm.softspec.queue2win.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public EditText username;
+    public EditText password;
+    public Button login;
+    public Button register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initComponents();
+
+    }
+
+    private void initComponents() {
+        username = (EditText) findViewById(R.id.login_edittext_username);
+        password = (EditText) findViewById(R.id.login_edittext_password);
+
+        
+
+
     }
 }
